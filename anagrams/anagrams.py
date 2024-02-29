59b1e52c16b48de4d78f92a4556943f1b200433a
harivmasoor <harimasoor@gmail.com>
2024-02-29 10:24:36 -0800

    Solved 'anagrams' (.py)

--- anagrams/anagrams.py
+++ anagrams/anagrams.py
@@ -1,12 +1,13 @@
 ​
 ​
+from collections import defaultdict
 def anagrams(s1, s2):
   return charCount(s1)==charCount(s2)
 ​
 def charCount(s):
-  hash = {}
+  hash = defaultdict(int)
   for c in s:
-    if not c in hash:
-      hash[c]=0
+    # if not c in hash:
+    #   hash[c]=0
     hash[c]+=1
   return hash
