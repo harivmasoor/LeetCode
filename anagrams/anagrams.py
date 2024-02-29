10f23822903afd76c264435dc0dd9066f66a3eb7
harivmasoor <harimasoor@gmail.com>
2024-02-29 10:22:48 -0800

    Solved 'anagrams' (.py)

---
+++ anagrams/anagrams.py
@@ -0,0 +1,12 @@
+​
+​
+def anagrams(s1, s2):
+  return charCount(s1)==charCount(s2)
+​
+def charCount(s):
+  hash = {}
+  for c in s:
+    if not c in hash:
+      hash[c]=0
+    hash[c]+=1
+  return hash
