008d617ca7275f0035dd7559fe7a0abf4faa45d2
harivmasoor <harimasoor@gmail.com>
2024-06-25 09:10:28 -0700

    Solved 'is prime' (.js)

--- is_prime/is_prime.js
+++ is_prime/is_prime.js
@@ -12,15 +12,15 @@ isPrime(2048); // -> false
 isPrime(1); // -> false
 isPrime(713); // -> false
 const isPrime = (n) => {
-  if(n ===1) return false;
-  for(let i = 2; i < n; i++){
-    if(n % i !== 0){
+  if(n === 1) return false;
+  for(i=2;i < n; i++){
+    if(n%i!==0){
       continue
     }
     else{
       return false
     }
-  }
+    }
   return true
 };
 
